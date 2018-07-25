68735b2ff8b7448896c062dd3b8c5e083ae209d5
TheFloW <theofficialflow1996@gmail.com>
2018-07-25 22:03:20 +0200

    Fixed boot on enso

--- shell.c
+++ shell.c
@@ -17,12 +17,14 @@
 */
 
 #include <psp2/kernel/modulemgr.h>
+#include <psp2/sysmodule.h>
 #include <taihen.h>
 
+static tai_hook_ref_t sceSysmoduleLoadModuleInternalWithArgRef;
 static tai_hook_ref_t scePafMiscIsVitaTVRef;
 static tai_hook_ref_t scePafMiscIsMultiControllerSupportedRef;
 
-static SceUID hooks[2];
+static SceUID hooks[3];
 
 static int scePafMiscIsVitaTVPatched() {
   TAI_CONTINUE(int, scePafMiscIsVitaTVRef);
@@ -34,17 +36,39 @@ static int scePafMiscIsMultiControllerSupportedPatched() {
   return 1;
 }
 
+static int sceSysmoduleLoadModuleInternalWithArgPatched(SceUInt32 id, SceSize args, void *argp, void *unk) {
+  int res = TAI_CONTINUE(int, sceSysmoduleLoadModuleInternalWithArgRef, id, args, argp, unk);
+
+  if (res >= 0 && id == SCE_SYSMODULE_INTERNAL_PAF) {
+    hooks[0] = taiHookFunctionImport(&scePafMiscIsVitaTVRef, "SceShell", 0x3D643CE8,
+                                     0xAF4FC3F4, scePafMiscIsVitaTVPatched);
+    hooks[1] = taiHookFunctionImport(&scePafMiscIsMultiControllerSupportedRef, "SceShell", 0x3D643CE8,
+                                     0x2C227FFD, scePafMiscIsMultiControllerSupportedPatched);
+  }
+
+  return res;
+}
+
 void _start() __attribute__ ((weak, alias("module_start")));
 int module_start(SceSize args, void *argp) {
-  hooks[0] = taiHookFunctionImport(&scePafMiscIsVitaTVRef, "SceShell", 0x3D643CE8,
-                                   0xAF4FC3F4, scePafMiscIsVitaTVPatched);
-  hooks[1] = taiHookFunctionImport(&scePafMiscIsMultiControllerSupportedRef, "SceShell", 0x3D643CE8,
-                                   0x2C227FFD, scePafMiscIsMultiControllerSupportedPatched);
+  tai_module_info_t info;
+  info.size = sizeof(tai_module_info_t);
+  if (taiGetModuleInfo("ScePaf", &info) >= 0) {
+    hooks[0] = taiHookFunctionImport(&scePafMiscIsVitaTVRef, "SceShell", 0x3D643CE8,
+                                     0xAF4FC3F4, scePafMiscIsVitaTVPatched);
+    hooks[1] = taiHookFunctionImport(&scePafMiscIsMultiControllerSupportedRef, "SceShell", 0x3D643CE8,
+                                     0x2C227FFD, scePafMiscIsMultiControllerSupportedPatched);
+  } else {
+    hooks[2] = taiHookFunctionImport(&sceSysmoduleLoadModuleInternalWithArgRef, "SceShell", 0x03FCF19D,
+                                     0xC3C26339, sceSysmoduleLoadModuleInternalWithArgPatched);
+  }
 
   return SCE_KERNEL_START_SUCCESS;
 }
 
 int module_stop(SceSize args, void *argp) {
+  if (hooks[2] >= 0)
+    taiHookRelease(hooks[2], sceSysmoduleLoadModuleInternalWithArgRef);
   if (hooks[1] >= 0)
     taiHookRelease(hooks[1], scePafMiscIsMultiControllerSupportedRef);
   if (hooks[0] >= 0)
